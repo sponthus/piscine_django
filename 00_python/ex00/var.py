1ed64c107fb1f22c95da02b2113a6382c9416796
Sarah Ponthus <sponthus@student.42lyon.fr>
2026-09-09 09:41:44 +0200

    00_python/ex00

---
+++ 00_python/ex00/var.py
@@ -0,0 +1,25 @@
+def my_var():
+    var_int = 42
+    var_str = "42"
+    var_str_verbose = "quarante-deux"
+    var_float = 42.0
+    var_bool = True
+    var_list = [42]
+    var_dict = {
+        42: 42
+    }
+    var_tuple = (42,)
+    var_set = set()
+
+    print(var_int, "has a type", type(var_int))
+    print(var_str, "has a type", type(var_str))
+    print(var_str_verbose, "has a type", type(var_str_verbose))
+    print(var_float, "has a type", type(var_float))
+    print(var_bool, "has a type", type(var_bool))
+    print(var_list, "has a type", type(var_list))
+    print(var_dict, "has a type", type(var_dict))
+    print(var_tuple, "has a type", type(var_tuple))
+    print(var_set, "has a type", type(var_set))
+
+if __name__ == '__main__':
+    my_var()
