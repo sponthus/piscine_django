2fd858bfd52bb38b188d01aa257063b23545f0d5
Sarah Ponthus <sponthus@student.42lyon.fr>
2026-09-09 10:18:58 +0200

    00_python/ex03

---
+++ 00_python/ex03/capital_city.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def capital_city(state: str):
+    """Gets and prints state's capital city from known dicts."""
+    states = {
+        "Oregon": "OR",
+        "Alabama": "AL",
+        "New Jersey": "NJ",
+        "Colorado": "CO"
+    }
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
+
+    if not isinstance(state, str) or not state:
+        print("Unknown state")
+        return
+
+    short_state = states.get(state, None)
+    if short_state is None:
+        print("Unknown state")
+        return
+
+    capital = capital_cities.get(short_state, None)
+    print(capital)
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 2:
+        sys.exit()
+
+    capital_city(args[1])
